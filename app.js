7525ffb47f95c8536ab2cf9e6ccee6d1ea57dfa7
2SivaDandamudi <tosivadandamudi@gmail.com>
2024-06-14 14:59:25 +0530

    added score display

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const player = document.querySelector('.player')
 const bot = document.querySelector('.bot')
 const resultDiv = document.querySelector('.result')
 const choices = ['rock', 'paper', 'scissors']
+const playerScoreText = document.querySelector('.playerScore')
+const computerScoreText = document.querySelector('.computerScore')
+let playerScore = 0
+let computerScore = 0
 
 function playGame (playerChoice) {
    
@@ -34,9 +38,13 @@ function playGame (playerChoice) {
     switch (result){
         case 'You Win' :
             resultDiv.classList.add('greenText')
+            playerScore++
+            playerScoreText.textContent = playerScore
             break
         case 'You Lose' :
             resultDiv.classList.add('redText')
+            computerScore++
+            computerScoreText.textContent = computerScore
             break
     }
 }
